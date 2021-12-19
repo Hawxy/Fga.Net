ddf3678c3990cd15aeca2be83a48721d8e08829b
JT <Hawxy@users.noreply.github.com>
2021-12-19 21:18:49 +0800

    Create build & push pipeline

---
+++ build/Build.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.Execution;
+using Nuke.Common.Git;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+[CheckBuildProjectConfigurations]
+[ShutdownDotNetAfterServerBuild]
+class Build : NukeBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main () => Execute<Build>(x => x.Compile);
+
+    [Solution(GenerateProjects = true)] readonly Solution Solution;
+
+    AbsolutePath SourceDirectory => RootDirectory / "src";
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            EnsureCleanDirectory(ArtifactsDirectory);
+        });
+
+    Target Restore => _ => _
+        .DependsOn(Clean)
+        .Executes(() =>
+        {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration("Release")
+                .EnableNoRestore());
+        });
+
+    Target NugetPack => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetPack(_ => _
+                .SetProject(Solution.Fga_Net)
+                .SetConfiguration("Release")
+                .EnableContinuousIntegrationBuild()
+                .SetOutputDirectory(ArtifactsDirectory));
+
+            DotNetPack(_ => _
+                .SetProject(Solution.Fga_Net_AspNetCore)
+                .SetConfiguration("Release")
+                .EnableContinuousIntegrationBuild()
+                .SetOutputDirectory(ArtifactsDirectory));
+        });
+
+    [Parameter("Nuget Api Key")]
+    readonly string NugetApiKey = string.Empty;
+    Target NugetPush => _ => _
+        .After(NugetPack)
+        .Requires(() => !string.IsNullOrEmpty(NugetApiKey))
+        .Executes(() =>
+        {
+            DotNetNuGetPush(_ => _
+                .SetTargetPath(ArtifactsDirectory / "*.nupkg")
+                .EnableSkipDuplicate()
+                .EnableNoSymbols()
+                .SetApiKey(NugetApiKey));
+        });
+
+}
